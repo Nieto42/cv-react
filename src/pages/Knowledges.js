bbcd452b57d9c7a0dba27cd32b8edab8b982aaab
Neto Pompeu <neto1@hotmail.fr>
2021-05-31 19:14:09 +0200

    Debug noFound Page React

---
+++ src/pages/Knowledges.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navigation from "../components/Navigation";
+
+const Knowledgs = () => {
+  return (
+    <div>
+      <Navigation />
+      compétences
+    </div>
+  );
+};
+
+export default Knowledgs;
---
+++ src/pages/Portfolio.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navigation from "../components/Navigation";
+
+const Portfolio = () => {
+  return (
+    <div>
+      <Navigation />
+      depuis le portofolio
+    </div>
+  );
+};
+
+export default Portfolio;
